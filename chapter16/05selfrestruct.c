d6108578047e72dc9b76c3aa79e05570bf62c1ce
20251642 신재범 <enjoy3568enjoy10000@gmail.com>
2026-06-10 02:31:40 +0000

    16

---
+++ chapter16/05selfrestruct.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct selfref list;
+
+int main(void)
+{
+    
+
+    
+    list* first = NULL, * second = NULL;
+    first = (list*) malloc( sizeof(list) );
+    second = (list*) malloc(sizeof(list));
+
+    first -> n = 100;
+    second -> n = 200;
+    first -> next = second -> next = NULL;
+
+    first -> next = second;
+
+    printf("%p \n", first);
+    printf("%d %p\n", first->n, first->next);
+    printf("%d\n\n", first->next->n);
+
+    printf("%p\n", second);
+    printf("%d %p\n", second->n, second->next);
+
+    free(first);
+    free(second);
+    
+    return 0;
+}
